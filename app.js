398e8fb907ace2b6407f10aa369b8dd12795e2f0
iliiiiil <110267226+iliiiiil@users.noreply.github.com>
2022-09-19 21:31:55 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
+//오브젝트
 const player = {
-    name = "user",
-    points = 10,
-    fat = true;
-}
+    name : "user",
+    points : 10,
+    fat : true
+};
+/*
 alert(player);
 player.points = 20;
 alert(player.points);
+console.log(player);
+*/
+
+//펑션
+function sayHello(name) { //argument
+    console.log("Hello! My name is " + name);
+}
+
+/*
+sayHello("red");
+sayHello("yellow");
+sayHello("blue");
+sayHello("purple");
+*/
+
+function plus(a, b) {
+    console.log(a + b);
+}
+
+//plus(2,5);
+
+
+//return값
+function calculationKrAge(ageOfForeigner) {
+    return ageOfForeigner + 2;
+}
+
+const krAge = calculationKrAge(96);
+
+console.log(krAge);
